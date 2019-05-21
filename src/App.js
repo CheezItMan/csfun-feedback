ba7821b8967ea3602d2976b4afc328774e1e8d8a
CheezItMan <chris@adadevelopersacademy.org>
2019-05-20 19:33:44 -0700

    updated App to render studentlist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
+import StudentList from './components/StudentList'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const studentList = [
+  {
+    id: 1,
+    name: 'Leroy Jinkins',
+    cohort: 111,
+    classroom: 'Crafters',
+    githubName: 'leroyjink',
+    email: 'leyroy@jinkins.com',
+  },
+  {
+    id: 2,
+    name: 'Devin Helmgren',
+    cohort: 100,
+    classroom: 'GMs',
+    githubName: 'dhelmgren',
+    email: 'dhelmgren@adadev.org',
+  },
+  {
+    id: 14,
+    name: 'Dan Roberts',
+    cohort: 111,
+    classroom: 'Crafties',
+    githubName: 'droberts',
+    email: 'dan@adadev.org',
+  },
+
+];
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      students: studentList,
+    }
+  }
+
+  editStudent = (id) => {
+    console.log(`Editing ${id}`);
+
+  }
+
+  deleteStudent = (id) => {
+    console.log(`Deleting ${id}`);
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+        <main className="App-body">
+          <StudentList students={this.state.students}
+            onDelete={this.deleteStudent}
+            onEdit={this.editStudent}
+          />
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
