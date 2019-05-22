0ec2c89addb9bfad3afbb97c339f561072ceff84
CheezItMan <chris@adadevelopersacademy.org>
2019-05-22 13:22:00 -0700

    firebase config added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// < !--The core Firebase JS SDK is always required and must be listed first-- >
+//  <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+//  <!--TODO: Add SDKs for Firebase products that you want to use
+//https://firebase.google.com/docs/web/setup#config-web-app -->
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOPvsEw3gxB6k2XYq6pKajRm-yP2gtZGE",
+  authDomain: "ada-csfun.firebaseapp.com",
+  databaseURL: "https://ada-csfun.firebaseio.com",
+  projectId: "ada-csfun",
+  storageBucket: "ada-csfun.appspot.com",
+  messagingSenderId: "866104156942",
+  appId: "1:866104156942:web:f69a5ec933b91817"
+};
+
+export default firebaseConfig;
