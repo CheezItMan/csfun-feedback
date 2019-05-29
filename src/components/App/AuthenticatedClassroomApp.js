c7674aaa457e7fc2e3f5dd6f3616e94df6e640e1
CheezItMan <chris@adadevelopersacademy.org>
2019-05-28 21:47:35 -0700

    Add StudentList to Authenticated App

--- src/components/App/AuthenticatedClassroomApp.js
+++ src/components/App/AuthenticatedClassroomApp.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import StudentList from '../StudentList';
+
 
 const AuthenticatedClassroomApp = (props) => {
   return (
     <div>
       <h2>Welcome {props.user.displayName} </h2>
+      <StudentList />
 
       <button className="btn btn-primary" onClick={() => props.signOut()}>
         Logout
