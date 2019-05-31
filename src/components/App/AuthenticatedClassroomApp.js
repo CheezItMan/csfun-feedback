630b28ff090dfd7d2967353c39c04ee7273dbfb3
CheezItMan <chris@adadevelopersacademy.org>
2019-05-31 10:55:40 -0700

    add data fields to Authenticated Classroom App

--- src/components/App/AuthenticatedClassroomApp.js
+++ src/components/App/AuthenticatedClassroomApp.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import StudentList from '../StudentList';
 
+const students = [
+  {
+    id: 1,
+    name: 'Pete',
+    github_name: 'p-master',
+  },
+  {
+    id: 47,
+    name: 'Sadie',
+    github_name: 's-master',
+  },
+  {
+    id: 100,
+    name: 'Ada',
+    github_name: 'ada',
+  },
+]
 
-const AuthenticatedClassroomApp = (props) => {
-  return (
-    <div>
-      <h2>Welcome {props.user.displayName} </h2>
-      <StudentList />
 
-      <button className="btn btn-primary" onClick={() => props.signOut()}>
-        Logout
+class AuthenticatedClassroomApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      students,
+      cohorts: [],
+      repos: [],
+      prs: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome {this.props.user.displayName} </h2>
+        <StudentList />
+
+        <button className="btn btn-primary" onClick={() => this.props.signOut()}>
+          Logout
       </button>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
 export default AuthenticatedClassroomApp;
