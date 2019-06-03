f4ce6ecbb2243b1a676fff5fd979e02545392510
CheezItMan <chris@adadevelopersacademy.org>
2019-06-03 14:49:33 -0700

    Added Student Provider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 
 import React, { Suspense } from 'react';
 import firebaseWrapper from './context/auth'
+import StudentsProvider from './providers/StudentsProvider';
+
 // Only load the components being used
 const AuthenticatedClassroomApp = React.lazy(() => import('./components/App/AuthenticatedClassroomApp'));
 const UnAuthenticatedClassroomApp = React.lazy(() => import('./components/App/UnAuthenticatedClassroomApp'));
 
 const App = (props) => {
-  console.log(props);
   const { user, signOut, signInWithGoogle, signInWithGithub } = props;
   if (user) {
     return (
       <Suspense fallback={<div>Loading</div>} >
-        <AuthenticatedClassroomApp user={user} signOut={signOut} fallback={<div>Loading</div>} />
+        <StudentsProvider>
+          <AuthenticatedClassroomApp user={user} signOut={signOut} fallback={<div>Loading</div>} />
+        </StudentsProvider>
       </Suspense>
     );
   }
