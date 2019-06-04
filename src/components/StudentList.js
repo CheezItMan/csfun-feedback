2627f2d4b9ce51b4cd25626ad0a5faa2175766c0
CheezItMan <chris@adadevelopersacademy.org>
2019-06-03 18:49:04 -0700

    studentList to functional component

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,54 +1,59 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Student from './Student';
+import { StudentsContext } from '../providers/StudentsProvider';
 
-class StudentList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      students: [],
-    }
-  }
-
-  renderStudents = () => {
-    const { students } = this.state;
-    return students.map((student) => {
-      return <Student key={student.id} {...student} handleDelete={this.props.onDelete} handleEdit={this.props.onEdit} />
-    });
-  }
-
-  render(props) {
+const renderStudents = (students, onEdit, onDelete) => {
+  if (students === null || students === undefined) {
     return (
-      <table className="Student-list">
-        <thead>
-          <tr>
-            <th>
-              Name
-        </th>
-            <th>
-              Cohort
-        </th>
-            <th>
-              Class
-        </th>
-            <th>
-              Github Name
-        </th>
-            <th>
-              Email
-        </th>
-            <th>
-              &nbsp;
-        </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderStudents()}
-        </tbody>
-      </table>
-    );
+      <tr>
+        <td></td><td></td><td></td><td></td><td></td>
+
+      </tr>
+    )
   }
+
+  return students.map((student) => {
+    return <Student key={student.id} {...student} handleDelete={onDelete} handleEdit={onEdit} />
+  });
+}
+
+
+const StudentList = (props) => {
+  const students = useContext(StudentsContext);
+  const { onEdit, onDelete } = props;
+
+  console.log(students);
+
+  return (
+    <table className="Student-list">
+      <thead>
+        <tr>
+          <th>
+            Name
+        </th>
+          <th>
+            Cohort
+        </th>
+          <th>
+            Class
+        </th>
+          <th>
+            Github Name
+        </th>
+          <th>
+            Email
+        </th>
+          <th>
+            &nbsp;
+        </th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderStudents(students, onEdit, onDelete)}
+      </tbody>
+    </table>
+  );
 };
 
 StudentList.propTypes = {
